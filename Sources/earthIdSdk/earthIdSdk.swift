e48038fccb614603e089b4e91384927b8274dac9
Vigneshwar [C] Ramasamy <ramlvign@amazon.com>
2023-02-19 17:29:30 +0530

    version 1.5 imported

--- Sources/earthIdSdk/earthIdSdk.swift
+++ Sources/earthIdSdk/earthIdSdk.swift
@@ -2,13 +2,14 @@ import Veriff
 import UIKit
 public struct earthIdSdk {
     public static func start(url: String,viewController:UIViewController) {
-        let veriff = VeriffSdk.shared
+        let veriff = VeriffSdk.shared;
+        let originalUrl = url.replacingOccurrences(of: "https://myearth.id/", with: "https://alchemy.veriff.com/v/");
         // Swift
         let branding = VeriffSdk.Branding()
         branding.logo = UIImage(named: "earthidlogo_round.png")
         let locale = Locale(identifier: "et")
         let configuration = VeriffSdk.Configuration(branding: branding, languageLocale: locale)
-        veriff.startAuthentication(sessionUrl: url, configuration: configuration,presentingFrom: viewController)
+        veriff.startAuthentication(sessionUrl: originalUrl, configuration: configuration,presentingFrom: viewController)
     }
     public init() {
     
